30d52a6bec00da52ce2249973e8d13584b35fcbc
joe <alexandre.fouellet@gmail.com>
2025-08-18 01:01:41 +0000

    test

--- updateJson.py
+++ updateJson.py
@@ -1,20 +1,28 @@
 import os
 import json
-import sys
+from copy import deepcopy
 
 FILE_PATH = "./data/data.json"
 data = None
-
 
 """
  json = { type: { category: [{card}...], category2...}, type2 ...}
 """
 def load_json_file(filepath):
     if not os.path.exists(filepath):
-        print(f"File '{filepath}' not found")
-        return sys.exit(1)
+        print(f"File '{filepath}' not found, created it!")
+        with open(filepath, 'w', encoding='utf-8') as f:
+            json.dump({}, f, indent=4)
+            return ()
     with open(filepath, 'r', encoding='utf-8') as f:
         return json.load(f)
+
+def update_json():
+    if not os.path.exists(FILE_PATH):
+        print(f"File '{FILE_PATH}' not found!")
+        return
+    with open(FILE_PATH, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
 
 COMMANDS = """
     Options:
@@ -25,25 +33,7 @@ COMMANDS = """
     [5] - REMOVE category
     [6] - REMOVE card
     [7] - UPDATE card
-    [8] - PRINT data
 """
-
-
-
-COMMANDS_LIST = []
-
-def print_options(string):
-    print(string)
-
-
-# add new type X
-# add new category X
-# add new card X
-# remove type x
-# remove category x
-# remove cards x
-# udpate cards 
-# print cards 
 
 # -------------------------------------------------------------------
 def add_dic(dic, key, val):
@@ -62,6 +52,9 @@ def update_dic(dic, updates):
     for k, v in updates.items():
         dic[k] = v
 # -------------------------------------------------------------------
+def print_options(string):
+    print(string)
+
 def get_user_input(string):
     print(string)
     return input("Type and press ENTER > ").strip()
@@ -79,9 +72,9 @@ def ask_yes_or_no(string):
         except:
             print("Select a number!")
 
-def user_string_match(options):
+def user_string_match(name, options):
     while True:
-        print(f"Select between those options: {options}")
+        print(f"Select between those {name}: {options}")
         user_input = input("Type then press [ENTER] > ").strip()
         if user_input not in options:
             print("Type one of the following options!")
@@ -94,8 +87,8 @@ def get_input_option(string, options_nbr):
         user_input = input("Select your option [x] and press ENTER > ").strip()
         try:
             user_option = abs(int(user_input))
-            if user_option > options_nbr:
-                print(f"Select between 0 and {options_nbr}")
+            if user_option > options_nbr or user_option == 0:
+                print(f"Select between 1 and {options_nbr}")
             else:
                 return user_option
         except:
@@ -114,98 +107,123 @@ def get_card_by_english(category, english_name):
     return None
 # -------------------------------------------------------------------
 
-# select a type
-# select a category
-# select a card
+def get_type():
+    if len(data) == 0:
+        print("There is no types yet, please create one first!")
+        return None
+   
+    return data[user_string_match("types", list(data.keys()))]
 
-# print and validate 
+def get_category():
+    selected_type = get_type()
+    if selected_type == None:
+        return None
+    
+    if len(selected_type) == 0:
+        print("This type has no category yet, please create one first!")
+        return None
+    
+    return selected_type[user_string_match("categories", list(selected_type.keys()))]
 
-# save json 
+# -------------------------------------------------------------------
 
 def update_card():
-    if len(data) == 0:
-        print("Nothing to update yet")
-        return
-
-    selected_type = user_string_match(data.keys()) 
-    if len(selected_type) == 0:
-        print("The type is empty, nothing to update!")
-        return 
+    selected_category = get_category()
+    if selected_category == None:
+        return "Could not update card"
     
-    selected_category = user_string_match(selected_type.keys())
     if len(selected_category) == 0:
-        print("Empty category, nothing to update")
-        return 
+        print("This category has no cards yet!")
+        return "Could not update card"
     
-    selected_english_name = user_string_match(get_english_keys(selected_category))
-    selected_card = get_card_by_english(selected_english_name)
+    selected_english_name = user_string_match("english names", get_english_keys(selected_category))
+    selected_card = get_card_by_english(selected_category, selected_english_name)
+    
+    new_card = deepcopy(selected_card)
+    for k, v in selected_card.items():
+        print(f"Selected Card [{k}: {v}]")
+        if ask_yes_or_no(f"Do you want to modify the {k} field?"):
+            while True:
+                new_value = get_user_input(f"Type the new {k} value")
+                print(f"New change -> {k}: {new_value}")
+                if ask_yes_or_no("Do you want to accept this modification?"):
+                    new_card[k] = new_value
+                    break
+                else:
+                    if ask_yes_or_no("Skip this field?"):
+                        break
+    if new_card != selected_card:
+        print(f"Card: {selected_card} -> {new_card}")
+        if ask_yes_or_no("Do you want to accept these changes?"):
+            update_dic(selected_card, new_card)
+            update_json()
+            return f"Card {selected_card["english"]} updated!"
 
-    # loop in each field, print and ask if want to update
-    # if yes apply changes and confirms
-    # last confirms at the end 
-    update_dic()
+    return "Could not update card"
 
+# -------------------------------------------------------------------
 
 def remove_type():
     if len(data) == 0:
         print("Nothing to remove yet")
-        return
+        return "Could not remove type"
     
-    type_to_remove = user_string_match(data.keys())
-    # you sure you want to delete and do the same with adding or updating
-    del_dic(data, type_to_remove)
+    type_to_remove = user_string_match("types",list(data.keys()))
+
+    if ask_yes_or_no(f"Are you sure you want to remove {type_to_remove}?"):
+        del_dic(data, type_to_remove)    
+        update_json()
+        return f"Type {type_to_remove} removed!"
+    
+    return "Could not remove type"
 
 def remove_category():
-    if len(data) == 0:
-        print("Nothing to remove yet")
-        return
+    selected_type = get_type()
+    if selected_type == None:
+        return "Could not remove category"
     
-    selected_type = user_string_match(data.keys()) 
     if len(selected_type) == 0:
-        print("The type is empty, nothing to delete!")
-        return 
+        print("This type has no category yet!")
+        return "Could not remove category"
     
-    selected_category = user_string_match(selected_type.keys())
-    del_dic(selected_type, selected_category)
-
+    selected_category = user_string_match("categories", list(selected_type.keys()))
+    if ask_yes_or_no(f"Are you sure you want to remove {selected_category}?"):
+        del_dic(selected_type, selected_category)
+        update_json()
+        return f"Category {selected_category} removed!"
+    
+    return "Could not remove category"
+    
 def remove_card():
-    if len(data) == 0:
-        print("Nothing to remove yet")
-        return
+    selected_category = get_category()
+    if selected_category == None:
+        return "Could not remove card"
     
-    selected_type = user_string_match(data.keys()) 
-    if len(selected_type) == 0:
-        print("The type is empty, nothing to delete!")
-        return 
-    
-    selected_category = user_string_match(selected_type.keys())
     if len(selected_category) == 0:
-        print("Empty category, nothing to delete")
-        return 
+        print("This category has no cards yet!")
+        return "Could not remove card"
     
-    selected_english_name = user_string_match(get_english_keys(selected_category))
-    selected_card = get_card_by_english(selected_english_name)
-
-    del_list(selected_card)
+    selected_english_name = user_string_match("english names",get_english_keys(selected_category))
+    selected_card = get_card_by_english(selected_category, selected_english_name)
+    if ask_yes_or_no(f"Are you sure you want to remove {selected_card}?"):
+        del_list(selected_card)
+        update_json()
+        return f"Card {selected_card} removed!"
+    
+    return "Could not remove card"
+    
 # -------------------------------------------------------------------
 def add_card():
-    if len(data) == 0:
-        print("There is no type or category yet, please create them first!")
-        return
+    selected_category = get_category()
+    if selected_category == None:
+        return "Could not add card"
     
-    selected_type = user_string_match(data.keys())
-    if len(selected_type) == 0:
-        print("This type has no category yet, please create one first!")
-        return 
-    
-    selected_category = user_string_match(selected_type.keys())
-
     new_card = {}
 
     english_word = get_user_input("Type the english word of the new card")
     if check_if_card_exists( selected_category, english_word):
         print("A card with this name exists already!")
-        return 
+        return "Could not add card"
     
     new_card["english"] = english_word
     new_card["spanish"] = get_user_input("Type the spanish word")
@@ -214,67 +232,86 @@ def add_card():
         new_card["image"] = get_user_input("Type the image link (with extension at the end)")
     
     add_list(selected_category, new_card)
+    update_json()
+
+    return f"Card {new_card} created!"
 
 def add_category():
-    if len(data) == 0:
-        print("There is no type to add category on, please create one first!")
-        return
-
-    selected_type = user_string_match(data.keys()) 
+    selected_type = get_type()
+    if selected_type == None:
+        return "Could not add category"
+    
     new_category = get_user_input("Type the new category name")
 
     if new_category in selected_type:
         print("A category with this name exists already")
-        return
+        return "Could not add category"
     
     add_dic(selected_type, new_category, [])
+    update_json()
+
+    return f"Category {new_category} created!"
 
 def add_type():
     new_type = get_user_input("Type the new type name")
 
     if new_type in data:
         print("A type with this name exists already")
-        return
+        return "Could not add type!"
     
     add_dic(data, new_type, {})
+    update_json()
 
+    return f"Type {new_type} created!"
 # -------------------------------------------------------------------
-# card is a dictionnary 
-def removeCards(category, cards):
-    for card in cards:
-        if card in category:
-            category.remove(card)
 
-# category is a string representing the key
-def removeCategories(type, categories):
-    for category in categories:
-        if category in type:
-            del type[category]
-
-# type is a string key
-def removeTypes(types):
-    for type in types:
-        if type in data:
-            del data[type]
-
-
-
-
-
+"""
 def handle_option(user_choice, options):
     for index, option in enumerate(options):
         if index == user_choice:
             option()
             break
+"""
+def handle_option(user_input):
+    result = None
+
+    match user_input:
+        case 1:
+            print("\nADD type \n")
+            result = add_type()
+        case 2:
+            print("\nADD category \n")
+            result = add_category()
+        case 3:
+            print("\nADD card \n")
+            result = add_card()
+        case 4:
+            print("\nREMOVE type \n")
+            result = remove_type()
+        case 5:
+            print("\nREMOVE category \n")
+            result = remove_category()
+        case 6:
+            print("\nREMOVE card \n")
+            result = remove_card()
+        case 7:
+            print("\nUPDATE card \n")
+            result = update_card()
+        case _:
+            pass
+
+    return result
 
 def main():
+    global data
     data = load_json_file(FILE_PATH)
-    #create empty data if no data or type or category 
+
     try:
         while True:
-            user_input = get_input_option(COMMANDS, 8)
-         
-            #handle_option(user_option)
+            user_input = get_input_option(COMMANDS, 7)
+            result = handle_option(user_input)
+            print("\n" + result)
+            input("\nPress Enter")
     except KeyboardInterrupt:
         pass
 
